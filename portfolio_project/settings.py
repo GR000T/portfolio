5b1da7acfd33a68ffbd994238fe40b49119c4668
GR000T <67423101+GR000T@users.noreply.github.com>
2022-08-07 17:05:22 +0530

    static changes

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -119,17 +119,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+'''STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-]
+]'''
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
-# Configure Django App for Heroku.
-import django_heroku
-django_heroku.settings(locals())
+#
