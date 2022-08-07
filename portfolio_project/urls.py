29a8aed35ff33f587c6d57746264df990ce5693b
GR000T <67423101+GR000T@users.noreply.github.com>
2022-08-07 16:43:16 +0530

    settings and url

--- portfolio_project/urls.py
+++ portfolio_project/urls.py
@@ -18,6 +18,9 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio_page.urls')),
